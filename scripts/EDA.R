81034d832a4d93f6bcbd85de11716983770ae1c3
WojtekBogucki <boguckiw@student.mini.pw.edu.pl>
2020-04-21 20:09:42 +0200

    testing functions from funModeling package

--- scripts/EDA.R
+++ scripts/EDA.R
@@ -4,6 +4,7 @@ library(funModeling)
 library(dplyr)
 
 
+# renaming columns --------------------------------------------------------
 JM1 <- getOMLDataSet(data.id = 1053)
 
 data <- na.omit((JM1)$data)
@@ -11,23 +12,57 @@ colnames(data) <- c("MC_line_count","MC_cyclomatic","MC_essential","MC_design","
                     "effort", "b", "t","line_code", "line_comment","line_blank","line_code_and_comment","unique_operators","unique_operands",
                     "total_operators","total_operands","flow_graph","TARGET")
 levels(data$TARGET) <- c(0, 1)
-
-data %>% filter(line_code!=0 & line_comment==0) %>% count()
-
 write.csv(data, file = "data/JM1_renamed.csv",row.names = FALSE)
+
+
+# EDA ---------------------------------------------------------------------
+colnames(data)
+View(status(data))
+di <- data_integrity(data)
+summary(di)
+print(di)
+View(profiling_num(data))
+cross_plot(data, target = "TARGET")
 summary(data)
 introduce(data = data)
 plot_intro(data)
-plot_missing(data)
-pc <- plot_correlation(data)
-plot_qq(data)
-plot_bar(data)
+
+# McCabe's measures correlation -------------------------------------------
+
+plot_correlation(data %>% select(MC_line_count,MC_cyclomatic,MC_essential,MC_design,TARGET))
+plot_correlation(data %>% select(-MC_line_count,-MC_cyclomatic,-MC_essential,-MC_design))
+
+# strong correlations -----------------------------------------------------
+
+correlation_table(data, "total_operators")
+plot(data$total_operators,data$num_oo)
+plot(data$b, data$volume)
+plot(data$t, data$effort)
+plot(data$total_operators, data$volume)
+
 plot_histogram(data)
-View(df_status(data))
-describe(data)
+plot_boxplot(data,by = "TARGET")
 freq(data)
-correlation_table(data, target = "line_code")
+
+# columns discretization --------------------------------------------------
+db <- discretize_get_bins(data, n_bins = 10)
+data_discrete <- discretize_df(data, db)
+cross_plot(data_discrete, target = "TARGET")
+plot_bar(data_discrete)
+
+# outliers ----------------------------------------------------------------
+describe(data$MC_line_count)
+count(data[data$MC_line_count>300,])
+plot_boxplot(data[data$MC_line_count<100,],by = "TARGET")
+plot_histogram(data[data$MC_line_count<100,])
+describe(data_po$flow_graph)
+# to test
+hampel_outlier(data$flow_graph)
+data_po <- prep_outliers(data,input = c('flow_graph'), type='stop', method='tukey')
+
 # to drop: t, b
+
+# data transformation -----------------------------------------------------
 plot_num(data %>% select(line_code), bins=50)
 plot_num(data %>% select(line_code) %>% mutate(line_code=log(line_code)), bins = 50)
 plot_num(data %>% select(difficulty) %>% mutate(difficulty=log(difficulty)), bins = 30)
@@ -35,8 +70,6 @@ plot_num(data %>% select(difficulty) %>% mutate(difficulty=log(difficulty)), bin
 data2 <- data %>% filter(line_code>0)
 View(df_status(data2))
 
-plot_num(data %>% mutate(lOCode=log(lOCode+1))%>%select(lOCode) ,bins=100)
-plot_prcomp(data %>% select(-defects))
 
 cross_plot(data, c("line_code"), target = "TARGET",)
 plot_boxplot(data %>% mutate(MC_line_count=log(MC_line_count))%>% select(MC_line_count, TARGET), by="TARGET")
