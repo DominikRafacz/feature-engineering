0fdbdc0e7fba5057a847845714c1fcc7b1a0f71f
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2020-05-02 16:31:54 +0200

    udpated packages needed by workflow

--- scripts/install_needed_packages.R
+++ scripts/install_needed_packages.R
@@ -1,14 +1,21 @@
 installed <- rownames(installed.packages())
 
-if (!"drake" %in% installed) install.packages("drake")
-if (!"OpenML" %in% installed) install.packages("OpenML")
-if (!"visNetwork" %in% installed) install.packages("visNetwork")
-if (!"lubridate" %in% installed) install.packages("lubridate")
-if (!"caret" %in% installed) install.packages("caret")
-if (!"dplyr" %in% installed) install.packages("dplyr")
-if (!"xgboost" %in% installed) install.packages("xgboost")
-if (!"mlr" %in% installed) install.packages("mlr")
-if (!"ranger" %in% installed) install.packages("ranger")
-if (!"gbm" %in% installed) install.packages("gbm")
+for (package in c("drake", 
+                  "OpenML",
+                  "farff",
+                  "visNetwork",
+                  "lubridate",
+                  "caret",
+                  "dplyr",
+                  "stringi",
+                  "mlr",
+                  "xgboost",
+                  "ranger",
+                  "gbm",
+                  #"bartMachine"
+                  )) {
+  if (!package %in% installed) install.packages(package)
+}
+
 
 rm(installed)
